b2b55e5a9decb22d9c49de6bb05d4b104bb250fc
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-12-27 19:53:05 +0530

    changes by shivanand

--- Frontend/src/components/Services.jsx
+++ Frontend/src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from './Card'
-import PrimaryBtn from './PrimaryBtn'
+import clean from '../assets/images/clean.webp'
+
 
 
 const Services = () => {
@@ -9,7 +10,7 @@ const Services = () => {
 
       <h1 className='text-center font-bold mb-4 mt-4 text-5xl'>Services</h1>
       <div className='md:flex md:justify-between md:items-center md:gap-8 px-4'>
-        <Card text="Plumbing" />
+        <Card text="Plumbing" img={clean} />
         <Card text="Electrition" />
         <Card text="Carpentry" />
         <Card text="Interior design" />
--- Frontend/src/components/Works.jsx
+++ Frontend/src/components/Works.jsx
@@ -4,7 +4,6 @@ const Works = () => {
   return (
     <div>
       <h1 className='text-center font-bold mb-4 mt-4 text-5xl'>How Its Works</h1>
-      
       <div className='h-[160px] w-[160px] shadow-lg border-2  rounded-full gap 3'></div>
       <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full gap 3'></div>
       <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full gap 3'></div>
