ee7b2f87f7b5e77cd3759154c7823fc3cc1ccb41
Anamika Pandey <149082695+anamika7015@users.noreply.github.com>
2024-12-27 20:09:04 +0530

    commit by anii

--- Frontend/src/components/Works.jsx
+++ Frontend/src/components/Works.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 const Works = () => {
   return (
     <div>
-      
-      <div className='h-[160px] w-[160px] shadow-lg border-2  rounded-full gap 3'></div>
-      <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full gap 3'></div>
-      <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full gap 3'></div>
+      <h1 className='text-center font-bold mb-4 mt-4 text-5xl'>How It Works</h1>
+      <div className='md:flex  md:justify-center md:items-center md:gap-56  '>
+        
+      <div className='h-[160px] w-[160px] shadow-lg border-2  rounded-full flex justify-center items-center '><h1 className='text-center'>Services choose Karein </h1></div>
+      <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full flex justify-center items-center  '><h1 className='text-center'>Professionals ko hire Karein</h1></div>
+      <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full flex justify-center items-center  '> <h1 className='text-center'>Kaam Complete karayein</h1></div>
+      </div>
     </div>
   )
 }
