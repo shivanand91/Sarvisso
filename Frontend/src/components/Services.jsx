55a9a0482a72d1ae7dea4eb4a929a008a4307911
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-12-27 22:04:27 +0530

    changes by me

--- Frontend/src/components/Services.jsx
+++ Frontend/src/components/Services.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Card from './Card'
 import clean from '../assets/images/clean.webp'
-
+import plumb from '../assets/images/plumb.webp'
+import electric from '../assets/images/electric.webp'
 
 
 const Services = () => {
   return (
     <div>
-
       <h1 className='text-center font-bold mb-4 mt-4 text-5xl'>Services</h1>
       <div className='md:flex md:justify-between md:items-center md:gap-8 px-4'>
-        <Card text="Plumbing" img={clean} />
-        <Card text="Electrition" />
-        <Card text="Carpentry" />
-        <Card text="Interior design" />
+        <Card text="Plumbing" img={plumb} />
+        <Card text="Electrition" img={electric} />
+        <Card text="Carpentry" img={clean} />
+        <Card text="Interior design" img={clean} />
         <div className='h-[250px] w-[250px] shadow-lg border-2 flex flex-col justify-center items-center m-auto rounded-lg mt-4'>
           <button className='h-[140px] w-[140px] bg-gray-200  rounded-md gap-3 flex items-center justify-center hover:bg-gray-300'>View All</button>
         </div>
