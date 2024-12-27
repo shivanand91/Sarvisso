1695251d2ac5edee5e21b62f97b25ef5675984c7
Anamika Pandey <149082695+anamika7015@users.noreply.github.com>
2024-12-27 18:41:48 +0530

    change by pandey

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,16 +4,10 @@ import Services from '../components/Services'
 
 const Home = () => {
   return (
-<<<<<<< HEAD
   <div>
      <Swiper />
      <Services/>
   </div>
-=======
-    <div className='min-h-screen'>
-      <Swiper />
-    </div>
->>>>>>> 3f904637dfe6b61db8a8f6f2aeba2600f2e7e040
   )
 }
 
