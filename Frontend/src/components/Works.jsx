6f0f7772671758994a775e2a3ddc99bb461d2840
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-12-27 22:21:04 +0530

    comminting by me

--- Frontend/src/components/Works.jsx
+++ Frontend/src/components/Works.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 const Works = () => {
   return (
-    <div>
+    <div className=''>
       <h1 className='text-center font-bold mb-4 mt-4 text-5xl'>How It Works</h1>
-      <div className='md:flex  md:justify-center md:items-center md:gap-56  '>
-        
-      <div className='h-[160px] w-[160px] shadow-lg border-2  rounded-full flex justify-center items-center '><h1 className='text-center'>Services choose Karein </h1></div>
-      <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full flex justify-center items-center  '><h1 className='text-center'>Professionals ko hire Karein</h1></div>
-      <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full flex justify-center items-center  '> <h1 className='text-center'>Kaam Complete karayein</h1></div>
+      <div className='flex flex-wrap justify-between px-4 md:items-center md:gap-56 mt-14 '>
+        <div className='h-[160px] w-[160px] shadow-lg border-2  rounded-full flex justify-center items-center '><h1 className='text-center'>Services choose Karein </h1></div>
+        <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full flex justify-center items-center  '><h1 className='text-center'>Professionals ko hire Karein</h1></div>
+        <div className='h-[160px] w-[160px] shadow-lg border-2 rounded-full flex justify-center items-center  '> <h1 className='text-center'>Kaam Complete karayein</h1></div>
       </div>
     </div>
   )
