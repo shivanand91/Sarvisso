52f2226e6ca5bbe8507d7d2c14840d78a5bcd6be
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-12-28 10:54:16 +0530

    error solved

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 import Swiper from '../components/Swiper'
 import Services from '../components/Services'
 import Works from '../components/Works'
-<<<<<<< HEAD
 import RegisterPopUp from '../components/RegisterPopUp'
-=======
 import Whyuse from '../components/Whyuse'
->>>>>>> 68ea93f885af8e54de9255a47499e9ff66c4f445
 
 const Home = () => {
   return (
