85b80e29d7329f4c4a01596ca92bfe7c4be1b47d
Anamika Pandey <149082695+anamika7015@users.noreply.github.com>
2024-12-30 22:19:49 +0530

    commit by pandey

--- Frontend/src/components/Whyuse.jsx
+++ Frontend/src/components/Whyuse.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 const Whyuse = () => {
   return (
-    <div className='flex justify-center items-center flex-col shadow-md border-2 m-4 pt-2 pb-2 rounded-lg px-6'>
-      <h1 className='text-center font-bold mb-4 mt-4 text-5xl '>Why Choose Us</h1>
-      <div className=' font-bold md:flex  md:justify-center md:items-center md:gap-48 text-2xl  text-center '>
-        <div><div className='h-[140px] w-[140px] gap-3 z-50 border-2 shadow-2xl rounded-md '></div><h1 className='w-[20px] text-center mt-4'>Verified Professionals</h1></div>
-        <div><div className='h-[140px] w-[140px] gap-3 z-50 border-2 shadow-2xl rounded-md'></div><h1 className='w-[15px] text-center mt-4'>On-Demand Services</h1></div>
-        <div> <div className='h-[140px] w-[140px] gap-3 z-50 border-2 shadow-2xl rounded-md'></div><h1 className='w-[20px] text-center mt-4'>Affordable Pricing</h1></div>
+    <div className='flex justify-center items-center  gap-3 shadow-md border-2 m-4 pt-2 pb-2 rounded-lg px-6  '>
+
+      <div className='flex   gap-3 '>
+        <div className='w-[725px] h-[500px] bg-white-500 shadow-lg  rounded-md border-2 '>
+
+        </div>
+        <div className='w-[725px] h-[500px] bg-blue-600 shadow-lg  rounded-md border-2'></div>
       </div>
+      <div className='bg-red-700 shadow-lg rounded-md w-[700px] h-[450px] z-50'></div>
     </div>
+
   )
 }
 
